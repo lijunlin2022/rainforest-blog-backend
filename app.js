04a7d512d7df61f5650ec352296bbf41c47aac7c
a-layfolk <1808150553@qq.com>
2021-03-17 14:37:01 +0800

    [feat] Delete public folder

--- app.js
+++ app.js
@@ -20,11 +20,6 @@ app.use(bodyparser({
 }))
 app.use(json())
 app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
-
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
-}))
 
 // logger
 app.use(async (ctx, next) => {
