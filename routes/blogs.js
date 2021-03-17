8ad7471b89741b1f11c1454b59ef2f4571a3bf21
a-layfolk <1808150553@qq.com>
2021-03-17 16:34:13 +0800

    [feat] Connection routes and database

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,33 +1,51 @@
 const router = require('koa-router')()
-
+const {
+  getList,
+  getDetail,
+  newBlog,
+  updateBlog,
+  delBlog
+} = require("../controller/blogs")
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 router.prefix('/api/blogs')
 
 router.get('/list', async (ctx, next) => {
-  // await ctx.render('index', {
-  //   title: 'Hello Koa 2!'
-  // })
-  ctx.body = "This is the list of blogs";
+  const author = ctx.query.author
+  const keyword = ctx.query.keyword
+  const listData = await getList(author, keyword)
+  ctx.body = new SuccessModel(listData)
 })
 
 router.get('/detail', async (ctx, next) => {
-  ctx.body = 'This is the detail of blogs'
+  const id = ctx.query.id
+  const data = await getDetail(id)
+  ctx.body = new SuccessModel(data)
 })
 
 router.post('/new', async (ctx, next) => {
-  ctx.body = {
-    title: 'This is the new of blogs'
-  }
+  const body = ctx.request.body
+  body.author = ctx.session.username
+  const data = await newBlog(body)
+  ctx.body = new SuccessModel(data)
 })
 
 router.post('/update', async (ctx, next) => {
-  ctx.body = {
-    title: 'This is the update of blogs'
+  const val = await updateBlog(ctx.query.id, ctx.request.body)
+  if (val) {
+      ctx.body = new SuccessModel()
+  } else {
+      ctx.body = new ErrorModel('更新博客失败')
   }
 })
 
 router.post('/del', async (ctx, next) => {
-  ctx.body = {
-    title: 'This is the del of blogs'
+  const id = ctx.query.id
+  const author = ctx.session.username
+  const val = await delBlog(id, author)
+  if (val) {
+    ctx.body = new SuccessModel()
+  } else {
+    ctx.body = new ErrorModel('删除博客失败')
   }
 })
 
