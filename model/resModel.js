eb768305247784300ff42679b05f27cab0ab23f9
a-layfolk <1808150553@qq.com>
2021-03-17 14:23:52 +0800

    [feat] Add a model that returns results

---
+++ model/resModel.js
@@ -0,0 +1,36 @@
+class BaseModel {
+  constructor(data, message) {
+    // 为了兼容传 对象 和 字符串
+    // 如果只传了字符串, 第一个参数为 data, 赋值给 message
+    if (typeof data === "string") {
+      this.message = data;
+      data = null;
+      message = null;
+    }
+    if (data) {
+      this.data = data;
+    }
+    if (message) {
+      this.message = message;
+    }
+  }
+}
+
+class SuccessModel extends BaseModel {
+  constructor(data, message) {
+    super(data, message);
+    this.errno = 0;
+  }
+}
+
+class ErrorModel extends BaseModel {
+  constructor(data, message) {
+    super(data, message);
+    this.errno = -1;
+  }
+}
+
+module.exports = {
+  SuccessModel,
+  ErrorModel
+};
