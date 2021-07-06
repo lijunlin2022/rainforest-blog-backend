299b23aa1df4ec2a23febd33a6e3f01044825ad5
a-layfolk <1808150553@qq.com>
2021-07-07 04:16:16 +0800

    [feat] add the feature for new and update notebook

--- controller/notebooks.js
+++ controller/notebooks.js
@@ -20,13 +20,63 @@ const getList = async (current, size, keyword) => {
     return await exec(sql);
 };
 
+/**
+ * @param {Number} id
+ * @returns
+ */
 const getDetail = async (id) => {
   let sql = `select * from notebooks where id = ${id}`;
   const rows = await exec(sql);
   return rows[0];
 }
 
+/**
+ * @param {Object} data
+ * @returns
+ */
+const add = async (data = {}) => {
+  const name = xss(data.name);
+  const description = xss(data.description);
+  const created_time = Date.now();
+  const updated_time = Date.now();
+
+  let sql = `insert into notebooks (name, description, created_time, updated_time) values (`
+            + escape(name) + `,`
+            + escape(description) + `,`
+            + `${created_time}` + `,`
+            + `${updated_time}` + `)`;
+
+  const insertData = exec(sql);
+  return {
+    id: insertData.id,
+  };
+}
+
+/**
+ * @param {Number} id
+ * @param {Object} data
+ * @returns
+ */
+const update = async (id, data = {}) => {
+  const name = xss(data.name);
+  const description = xss(data.description);
+  const updated_time = Date.now();
+
+  let sql = `update notebooks set `
+            + `name = ` + escape(name) + `,`
+            + `description = ` + escape(description) + `,`
+            + `updated_time = ${updated_time} `
+            + `where id = ${id}`;
+  const updateData = await exec(sql);
+  if (updateData.affectedRows > 0) {
+      return true;
+  }
+  return false;
+}
+
 module.exports = {
   getList,
   getDetail,
+  add,
+  update,
 };
--- routes/notebooks.js
+++ routes/notebooks.js
@@ -1,5 +1,5 @@
 const router = require("koa-router")();
-const { getList, getDetail } = require("../controller/notebooks");
+const { getList, getDetail, add, update } = require("../controller/notebooks");
 const { SuccessModel, ErrorModel } = require("../model/resModel");
 router.prefix("/notebook");
 
@@ -14,6 +14,19 @@ router.get("/list", async (ctx, next) => {
 router.get("/detail",async (ctx, next) => {
   const id = ctx.query.id;
   const data = await getDetail(id);
+  ctx.body = new SuccessModel(data);
+});
+
+router.post("/new", async (ctx, next) => {
+  const body = ctx.request.body;
+  const data = await add(body);
+  ctx.body = new SuccessModel(data);
+});
+
+router.post("/update", async (ctx, next) => {
+  const id = ctx.query.id;
+  const body = ctx.request.body;
+  const data = await update(id, body);
   ctx.body = new SuccessModel(data);
 });
 
