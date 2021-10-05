671880d2c7a2a00dfc0336d4fd092b7f2b465d85
a-layfolk <1808150553@qq.com>
2021-10-05 21:30:11 +0800

    [delete] 删除原本 MySQL 的代码

--- middleware/loginCheck.js
+++
@@ -1,9 +0,0 @@
-const { ErrorModel } = require("../model/resModel");
-
-module.exports = async (ctx, next) => {
-  if (ctx.session.username) {
-    await next();
-    return;
-  }
-  ctx.body = new ErrorModel(401, "未登录");
-}
